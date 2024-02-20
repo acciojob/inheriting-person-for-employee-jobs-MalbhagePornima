c404d2e78929102fbdef1759be5872d706709a47
MalbhagePornima <142093558+MalbhagePornima@users.noreply.github.com>
2024-02-20 15:58:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,12 +12,19 @@ function Employee(name, age, jobTitle) {
 	this.jobTitle=jobTitle;
 }
 Employee.prototype=Object.create(Person.prototype);
-//Employee.prototype.constructor=Employee;
+Employee.prototype.constructor=Employee;
 
 Employee.protoype.jobGreet=function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
-}
+};
+
+const person=new Person('Alice',25);
+person.greet();
+
+const employee=new Employee('Bob',30,'Manager');
+employee.greet();
+employee.jobGreet();
 
 // Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
+//window.Person = Person;
+//window.Employee = Employee;
